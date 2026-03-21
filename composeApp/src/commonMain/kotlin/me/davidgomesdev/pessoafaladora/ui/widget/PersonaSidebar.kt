02fef024c85822b2efab0a9cc6fa87bb6a42af23
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2026-03-21 15:24:23 +0000

    chore: reorder personas

--- composeApp/src/commonMain/kotlin/me/davidgomesdev/pessoafaladora/ui/widget/PersonaSidebar.kt
+++ composeApp/src/commonMain/kotlin/me/davidgomesdev/pessoafaladora/ui/widget/PersonaSidebar.kt
@@ -37,9 +37,9 @@ enum class PersonaCategory(val label: String) {
 
 enum class Persona(val displayName: String, val category: PersonaCategory) {
     FERNANDO_PESSOA("Fernando Pessoa", PersonaCategory.ORTONIMO),
+    ALBERTO_CAEIRO("Alberto Caeiro", PersonaCategory.HETERONIMO),
     ALVARO_DE_CAMPOS("Álvaro de Campos", PersonaCategory.HETERONIMO),
     RICARDO_REIS("Ricardo Reis", PersonaCategory.HETERONIMO),
-    ALBERTO_CAEIRO("Alberto Caeiro", PersonaCategory.HETERONIMO),
     BERNARDO_SOARES("Bernardo Soares", PersonaCategory.SEMI_HETERONIMO),
 }
 

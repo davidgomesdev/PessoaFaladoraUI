ac85a47cb001320c1b2371489a2fdb3e2673bd2a
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2026-03-21 15:24:25 +0000

    fix: send button improperly placed on mobile web

--- composeApp/src/commonMain/kotlin/me/davidgomesdev/pessoafaladora/ui/service/Config.kt
+++ composeApp/src/commonMain/kotlin/me/davidgomesdev/pessoafaladora/ui/service/Config.kt
@@ -2,3 +2,4 @@ package me.davidgomesdev.pessoafaladora.ui.service
 
 expect fun getPessoaHost(): String
 expect fun isDevMode(): Boolean
+expect fun isMobileDevice(): Boolean
--- composeApp/src/jsMain/kotlin/me/davidgomesdev/pessoafaladora/ui/service/Config.kt
+++ composeApp/src/jsMain/kotlin/me/davidgomesdev/pessoafaladora/ui/service/Config.kt
@@ -5,3 +5,9 @@ actual fun getPessoaHost(): String =
 
 actual fun isDevMode(): Boolean =
     (js("window.IS_DEV_MODE") as? String) != "false"
+
+actual fun isMobileDevice(): Boolean {
+    val userAgent = js("navigator.userAgent") as String
+    return userAgent.contains(Regex("Mobile|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini"))
+}
+
